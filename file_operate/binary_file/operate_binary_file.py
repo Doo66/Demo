cf7cd71db39bca9df0072ce137d9c089722f71af
hinata <doo_magic@126.com>
2020-05-12 14:51:56 +0800

    python file operate

---
+++ file_operate/binary_file/operate_binary_file.py
@@ -0,0 +1,17 @@
+def main():
+    try:
+        with open('bit.jpg', 'rb') as fs1:
+            data = fs1.read()
+            # print(type(data)) # 数据类型
+        with open('newbit.jpg', 'wb') as fs2:
+            fs2.write(data)
+    except FileNotFoundError as e:
+        print("指定的文件无法打开!")
+    except IOError as e:
+        print("读写文件时出现错误!")
+    print("程序执行结束!")
+    
+if __name__ == '__main__':
+    main()
+else:
+    print("error")
